4fea7e317dd86f6a0e75306a98860c77f4132efd
zerosalife <zerosalife@gmail.com>
2016-02-04 17:03:59 -0600

    Test that map code generates unique codes

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Map, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:map1) { create(:map) }
+  let(:map2) { create(:map) }
+
+  it 'has unique code' do
+    expect(map1.code).not_to eq(map2.code)
+  end
 end
