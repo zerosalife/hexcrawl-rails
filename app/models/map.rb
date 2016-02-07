00f454c4f3ad637b5aaa73c39bc88389893de30b
zerosalife <zerosalife@gmail.com>
2016-02-07 14:42:20 -0600

    Automatically generate Tiles when creating Maps

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,11 +1,12 @@
 require 'set'
 
 class Map < ActiveRecord::Base
-  before_validation :create_code, on: :create
   has_many :tiles, dependent: :destroy
+  before_validation :create_code, :generate_tiles, on: :create
 
   CODE_CHARS = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
   CODE_LENGTH = 4
+  MAP_DIM_SIZE = 8
 
   private
 
@@ -15,6 +16,14 @@ class Map < ActiveRecord::Base
       return if self.class.where(code: self.code).empty?
     end
     raise "Couldn't find a unique room number :( We're running out of search space, better optimize"
+  end
+
+  def generate_tiles
+    (0...MAP_DIM_SIZE).each do |x|
+      (0...MAP_DIM_SIZE).each do |y|
+        self.tiles << Tile.create(x_coord: x, y_coord: y, map_id: self.id)
+      end
+    end
   end
 
   def generate_code
--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe Map, type: :model do
   it 'has unique code' do
     expect(map1.code).not_to eq(map2.code)
   end
+
+  it 'is square with a side length of MAP_DIM_SIZE tiles' do
+    expect(map1.tiles.count).to eq Map::MAP_DIM_SIZE * Map::MAP_DIM_SIZE
+  end
 end
