4b3ec765b9e7b23978d0cb35c3a2b701ce27327a
zerosalife <zerosalife@gmail.com>
2016-02-07 14:41:12 -0600

    Validate presence of map, tile coordinates
    - Create a valid tile factory

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,3 +1,6 @@
 class Tile < ActiveRecord::Base
   belongs_to :map
+  validates :map, presence: true
+  validates :x_coord, presence: true
+  validates :y_coord, presence: true
 end
---
+++ spec/factories/tile.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :tile do
+    x_coord 0
+    y_coord 0
+    map
+  end
+end
