09a57d8241d46ebe9fa7aff116a07b3d1190448e
zerosalife <zerosalife@gmail.com>
2016-02-12 09:22:19 -0600

    Pass correct params in Tiles controller spec

--- spec/controllers/tiles_controller_spec.rb
+++ spec/controllers/tiles_controller_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe TilesController, type: :controller do
 
   describe "GET #show" do
+    let(:map) { create(:map) }
+    let(:tile) { create(:tile, map: map) }
+
     it "returns http success" do
-      get :show
+      get :show, {code: map.code, id: tile.id}
       expect(response).to have_http_status(:success)
     end
   end
