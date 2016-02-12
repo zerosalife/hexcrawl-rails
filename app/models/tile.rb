a6ab015851376ae9a9887338ebfa862330463b29
zerosalife <zerosalife@gmail.com>
2016-02-12 13:24:09 -0600

    Add coords_string method to Tiles
    - Use it to replace code in the maps show view that did the same thing

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -3,4 +3,8 @@ class Tile < ActiveRecord::Base
   validates :map, presence: true
   validates :x_coord, presence: true
   validates :y_coord, presence: true
+
+  def coords_string
+    sprintf("%02d%02d", x_coord, y_coord)
+  end
 end
--- spec/models/tile_spec.rb
+++ spec/models/tile_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe Tile, type: :model do
+
+  describe '#coords_string' do
+    let(:map) { create :map }
+
+    it 'returns the coordinates formatted as a string' do
+      tile = map.tiles.first
+      expect(tile.coords_string).to eq "0000"
+    end
+  end
+end
