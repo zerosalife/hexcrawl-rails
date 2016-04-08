f1ddec86d002e3557c6ffa282bfac6857b1a0c23
zerosalife <zerosalife@gmail.com>
2016-04-08 14:54:20 -0500

    Flood fill to generate biome regions

--- app/models/map.rb
+++ app/models/map.rb
@@ -4,10 +4,13 @@ class Map < ActiveRecord::Base
   has_many :tiles, -> { order 'x_coord ASC, y_coord ASC' }, dependent: :destroy, autosave: true
   validates_associated :tiles
   before_validation :create_code, :generate_tiles, on: :create
+  # Need Tile's data to be realized, for nowwwwwwwwww
+  after_create :fill_regions, on: :create
 
   CODE_CHARS = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z).freeze
   CODE_LENGTH = 4
   MAP_DIM_SIZE = 8
+  DISTINCT_REGIONS = 3
 
   def find_tile_at(x: 0, y: 0)
     tiles.find_by(x_coord: x, y_coord: y)
@@ -28,6 +31,20 @@ class Map < ActiveRecord::Base
       (0...MAP_DIM_SIZE).each do |x|
         self.tiles.build(x_coord: x, y_coord: y)
       end
+    end
+  end
+
+  def fill_regions
+    frontier = self.tiles.shuffle.take DISTINCT_REGIONS
+    until frontier.empty?
+      current_tile = frontier.shift
+      current_tile.filled = true
+      current_tile.save
+      unfilled_neighbors = current_tile.neighbors.select { |n| !n.filled? }
+      # exclude unfilled neighbors if they're already in the frontier
+      unfilled_neighbors = unfilled_neighbors.select { |n| !frontier.include? n }
+      unfilled_neighbors.map { |n| n.data['biome'] = current_tile.data['biome'] }
+      frontier.push(*unfilled_neighbors)
     end
   end
 
--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Map, type: :model do
   it 'is square with a side length of MAP_DIM_SIZE tiles' do
     expect(map1.tiles.count).to eq Map::MAP_DIM_SIZE * Map::MAP_DIM_SIZE
   end
+
+  it 'has DISTINCT_REGIONS or fewer distinct regions' do
+    map1.reload
+    expect(map1.tiles.map { |n| n.data['biome'] }.uniq.count).to be <= Map::DISTINCT_REGIONS
+    # Test both generated maps to develop some statistical certainty
+    map2.reload
+    expect(map2.tiles.map { |n| n.data['biome'] }.uniq.count).to be <= Map::DISTINCT_REGIONS
+  end
 end
